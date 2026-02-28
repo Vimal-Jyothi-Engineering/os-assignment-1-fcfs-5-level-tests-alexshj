8b960ad4b6db7f7ea3e5f0cee2d50644010b7f20
Alex Shaju <alexshaju202006@gmail.com>
2026-02-28 14:16:34 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -18,7 +18,7 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    /* Stable sort by arrival time (FCFS) */
+    /* Stable FCFS sort by arrival time */
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (p[j].arrival > p[j + 1].arrival) {
@@ -32,17 +32,16 @@ int main() {
     int current_time = 0;
 
     for (int i = 0; i < n; i++) {
-        if (current_time < p[i].arrival) {
+        if (current_time < p[i].arrival)
             current_time = p[i].arrival;
-        }
 
-        int start_time = current_time;
-        int completion_time = start_time + p[i].burst;
+        int start = current_time;
+        int completion = start + p[i].burst;
 
-        p[i].waiting = start_time - p[i].arrival;
-        p[i].turnaround = completion_time - p[i].arrival;
+        p[i].waiting = start - p[i].arrival;
+        p[i].turnaround = completion - p[i].arrival;
 
-        current_time = completion_time;
+        current_time = completion;
     }
 
     double total_wt = 0, total_tat = 0;

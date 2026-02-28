be0d579a1d4f71ca15909cbdde73d37a3d76e5af
Alex Shaju <alexshaju202006@gmail.com>
2026-02-28 13:05:01 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -7,7 +7,6 @@ typedef struct {
     int burst;
     int waiting;
     int turnaround;
-    int index;   // original input position
 } Process;
 
 int main() {
@@ -18,10 +17,9 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
-        p[i].index = i;  // store original order
     }
 
-    // Sort by arrival time (stable FCFS)
+    // Sort by arrival time (FCFS order)
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (p[j].arrival > p[j + 1].arrival) {
@@ -36,34 +34,28 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         if (current_time < p[i].arrival) {
-            current_time = p[i].arrival;
+            current_time = p[i].arrival; // CPU idle
         }
 
         current_time += p[i].burst;
 
-        int completion = current_time;
-        p[i].turnaround = completion - p[i].arrival;
+        int completion_time = current_time;
+        p[i].turnaround = completion_time - p[i].arrival;
         p[i].waiting = p[i].turnaround - p[i].burst;
-    }
-
-    // Restore original order
-    Process result[n];
-    for (int i = 0; i < n; i++) {
-        result[p[i].index] = p[i];
     }
 
     double total_wt = 0, total_tat = 0;
 
     printf("Waiting Time:\n");
     for (int i = 0; i < n; i++) {
-        printf("%s %d\n", result[i].pid, result[i].waiting);
-        total_wt += result[i].waiting;
+        printf("%s %d\n", p[i].pid, p[i].waiting);
+        total_wt += p[i].waiting;
     }
 
     printf("Turnaround Time:\n");
     for (int i = 0; i < n; i++) {
-        printf("%s %d\n", result[i].pid, result[i].turnaround);
-        total_tat += result[i].turnaround;
+        printf("%s %d\n", p[i].pid, p[i].turnaround);
+        total_tat += p[i].turnaround;
     }
 
     printf("Average Waiting Time: %.2f\n", total_wt / n);

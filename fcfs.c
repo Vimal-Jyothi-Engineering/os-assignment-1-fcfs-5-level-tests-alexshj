ef32ed7f59dbfbd2ccb3aacd2bb3b38bccea68be
Alex Shaju <alexshaju202006@gmail.com>
2026-02-28 12:12:15 +0530

    Create fcfs.c
    Create fcfs.c in root directory

---
+++ fcfs.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char pid[10];
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+} Process;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    Process p[n];
+
+    // Read process details
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+    }
+
+    // Sort processes by arrival time (FCFS)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].arrival > p[j + 1].arrival) {
+                Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    // Compute waiting time and turnaround time
+    int current_time = 0;
+    for (int i = 0; i < n; i++) {
+        if (current_time < p[i].arrival) {
+            current_time = p[i].arrival;
+        }
+        p[i].waiting = current_time - p[i].arrival;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+        current_time += p[i].burst;
+    }
+
+    // Compute averages
+    double total_wt = 0, total_tat = 0;
+    for (int i = 0; i < n; i++) {
+        total_wt += p[i].waiting;
+        total_tat += p[i].turnaround;
+    }
+
+    // Print output EXACTLY as required
+    printf("Waiting Time:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].waiting);
+    }
+
+    printf("Turnaround Time:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].turnaround);
+    }
+
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+
+    return 0;
+}

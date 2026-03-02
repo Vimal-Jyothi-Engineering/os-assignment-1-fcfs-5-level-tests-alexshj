2babdb78b7465e9a07beac543dc424d128d32efe
Alex Shaju <alexshaju202006@gmail.com>
2026-03-02 17:23:50 +0530

    Update fcfs.c

--- src/fcfs.c
+++ src/fcfs.c
@@ -7,7 +7,6 @@ typedef struct {
     int burst;
     int waiting;
     int turnaround;
-    int original_index;
 } Process;
 
 int main() {
@@ -18,10 +17,9 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
-        p[i].original_index = i;
     }
 
-    // Sort by arrival time (bubble sort)
+    // Sort by arrival time
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (p[j].arrival > p[j+1].arrival) {
@@ -38,20 +36,9 @@ int main() {
         if (current_time < p[i].arrival) {
             current_time = p[i].arrival;
         }
-        p[i].waiting = current_time - p[i].arrival;
-        p[i].turnaround = p[i].waiting + p[i].burst;
         current_time += p[i].burst;
-    }
-
-    // Restore original input order for printing
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].original_index > p[j+1].original_index) {
-                Process temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
-            }
-        }
+        p[i].turnaround = current_time;         // TAT = completion time
+        p[i].waiting = p[i].turnaround - p[i].burst; // WT = TAT - burst
     }
 
     // Compute averages
@@ -61,7 +48,7 @@ int main() {
         total_tat += p[i].turnaround;
     }
 
-    // Print EXACTLY in required format
+    // Print in sorted order
     printf("Waiting Time:\n");
     for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].waiting);

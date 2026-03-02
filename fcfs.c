9babd8cd206596e61dc0264cb35e2b9ace19d1e0
Alex Shaju <alexshaju202006@gmail.com>
2026-03-02 17:56:54 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -6,7 +6,6 @@ typedef struct {
     int burst;
     int waiting;
     int turnaround;
-    int original_index;
 } Process;
 
 int main() {
@@ -17,14 +16,11 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
-        p[i].original_index = i;
     }
 
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].arrival > p[j + 1].arrival ||
-               (p[j].arrival == p[j + 1].arrival &&
-                p[j].original_index > p[j + 1].original_index)) {
+            if (p[j].arrival > p[j + 1].arrival) {
                 Process temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
@@ -38,13 +34,9 @@ int main() {
         if (current_time < p[i].arrival)
             current_time = p[i].arrival;
 
-        int start = current_time;
-        int completion = start + p[i].burst;
-
-        p[i].waiting = start - p[i].arrival;
-        p[i].turnaround = completion - p[i].arrival;
-
-        current_time = completion;
+        p[i].waiting = current_time - p[i].arrival;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+        current_time += p[i].burst;
     }
 
     double total_wt = 0, total_tat = 0;
